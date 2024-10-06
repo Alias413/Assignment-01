4ff16f9eac92e516bd11c4fe3af8be936f9d2c46
Phyth <Phyth@LAPTOP-T6QC85B5>
2024-10-05 22:13:13 -0400

    Fixed Looping Error

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -45,6 +45,7 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+	return 1;
 }
 
 void subtract() {
@@ -55,4 +56,5 @@ void subtract() {
 	scanf_s("%lf", &num2);
 	result = num1 - num2;
 	printf("%lf - %lf = %lf\n", num1, num2, result);
+	return 1;
 }
